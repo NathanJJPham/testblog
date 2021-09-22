4d16d92888b5238eaf8ea2180a4d8bf09c4069ee
NathanJJPham <nathan.pham@kin.com>
2021-09-22 11:31:41 -0400

    Db migrate matches, added to schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_21_191133) do
+ActiveRecord::Schema.define(version: 2021_09_22_152842) do
+
+  create_table "matches", force: :cascade do |t|
+    t.string "format"
+    t.string "time"
+    t.string "court"
+    t.string "score"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
